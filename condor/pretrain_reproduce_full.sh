7c0d91a29304da4a0234b329d142f12a2052c2fd
Po-Wei Huang <po-wei.huang@exeter.ox.ac.uk>
2026-08-14 14:52:54 +0100

    Clean up; intermediate step of making yaml to make it smoothly run in multi gpu cases(Not finish)

--- condor/pretrain_reproduce_full.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Executable launched by HTCondor for the reproduce_full pretraining run.
-set -euo pipefail
-
-source /data/snoplus/weiiiiiii/aiproj/driftmtplm/miniforge/etc/profile.d/conda.sh
-conda activate /data/snoplus/weiiiiiii/aiproj/driftmtplm/.venv
-
-exec python /home/huangp/aiproj/driftmtplm/third_party/mtp-lm/litgpt/pretrain.py \
-    --config=/home/huangp/aiproj/driftmtplm/third_party/mtp-lm-patch/config_hub/pretrain/reproduce_full.yaml
